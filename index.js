dc80b8067d0a1243efb50c03242ea3861aa4437f
Raaiyaann <moh.raiyan99@gmail.com>
2025-06-05 15:42:58 +0700

    menambahkan script js untuk card wisata

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const card = document.getElementsByClassName('card');
-const cardContent = document.getElementsByClassName('container-card-items');
-
+fetch('wisata.json')
+.then(response => response.json())
+.then(data => {
+    data.forEach(wisata => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+             <img src="${wisata.image}" alt="${wisata.name}" />
+              <div class="card-body">
+                <h3 class="property-name">${wisata.name}</h3>
+                <p class="location">${wisata.location}</p>
+                <p class="rating"> ${wisata.rating}</p>
+                <p class="price">${wisata.price}</p>
+              </div>
+        `;
+        document.getElementById('propertyCards').appendChild(card);
+    });
+})
+.catch(err => console.error("Gagal mengambil data:", err));
